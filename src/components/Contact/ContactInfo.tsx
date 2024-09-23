f34d1b35f2d50883677a82297f8cfd4c14f81a85
Ainjie <ainjiejomaa@gmail.com>
2024-09-23 22:25:38 +0200

    version actuelle

--- src/components/Contact/ContactInfo.tsx
+++ src/components/Contact/ContactInfo.tsx
@@ -3,10 +3,8 @@ import React from "react";
 export function ContactInfo() {
   return (
     <div className="d-flex flex-column align-items-center">
-      <h3>Coordonnées</h3>
-      <p className="m-0">Téléphone : 05652382829</p>
-      <p>Mail: urps971@gmail.com </p>
-      <p>adresse de urps-mk guadeloupe</p>
+      <p className="m-0">في حال وجود اي خلل في المنصة التواصل معنا على</p>
+      <p> joumaaaliofficiel@outlook.fr</p>
     </div>
   );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,19 +7,9 @@ import style from "./home.module.scss";
 const HomePage = () => {
   return (
     <div className={style.home}>
-      <div className={style.carouselWrapper}>
-        <CarouselComponent />
-      </div>
-      <button type="button" class="btn btn-primary" direction>
-        لوضع اعلان اضغط هنا
-      </button>
-      <button type="button" class="btn btn-primary" direction>
-        للاطلاع على الاعلانات الموجودة اضغط هنا
-      </button>
-      <div className="w-100 d-flex  flex-column align-items-center">
-        <About />
-        <ImageGallery />
-      </div>
+      <div className={style.carouselWrapper}></div>
+      <About />
+      <div className="w-100 d-flex  flex-column align-items-center"></div>
     </div>
   );
 };
