d76ed6606cbae79eda95be04c71a32856149c5c8
joumaaAli <ali.joumaa@net.usj.edu.lb>
2024-09-24 23:59:38 +0200

    merged

--- src/pages/my-house/index.tsx
+++ src/pages/my-house/index.tsx
@@ -124,48 +124,6 @@ const MyHousesPage = () => {
 
   const columns = [
     {
-<<<<<<< HEAD
-      name: "Name",
-      selector: (row: any) => row.name || "",
-      sortable: true,
-    },
-    {
-      name: "Address",
-      selector: (row: any) => row.address || "",
-      sortable: true,
-    },
-    {
-      name: "Phone Number",
-      selector: (row: any) =>
-        row?.phoneNumber ? (
-          <div
-            style={{
-              direction: "ltr",
-              textAlign: "left",
-              margin: 0,
-            }}
-          >
-            {row.phoneNumber}
-          </div>
-        ) : (
-          ""
-        ),
-      sortable: true,
-    },
-    {
-      name: "Space For People",
-      selector: (row: any) => row.spaceForPeople || "",
-      sortable: true,
-    },
-    {
-      name: "Taken",
-      selector: (row: any) => (row.taken ? "نعم" : "كلا"),
-      sortable: true,
-    },
-    {
-      name: "Region",
-      selector: (row: any) => row.region?.name || "",
-=======
       name: "الاسم",
       selector: (row: HouseType) => row.name || "",
       sortable: true,
@@ -178,7 +136,6 @@ const MyHousesPage = () => {
     {
       name: "المنطقة",
       selector: (row: HouseType) => row.region?.name || "",
->>>>>>> origin/my-houses
       sortable: true,
     },
     {
@@ -194,7 +151,7 @@ const MyHousesPage = () => {
     {
       name: "رقم الهاتف",
       cell: (row: HouseType) => (
-          <a href={`tel:${row.phoneNumber}`}>{row.phoneNumber}</a>
+        <a href={`tel:${row.phoneNumber}`}>{row.phoneNumber}</a>
       ),
       sortable: true,
     },
@@ -202,10 +159,9 @@ const MyHousesPage = () => {
       name: "",
       button: true,
       minWidth: "400px",
-<<<<<<< HEAD
-      cell: (row: any) => (
+      cell: (row: HouseType) => (
         <Row className="w-100">
-          <Col>
+          <Col className={style.tableRow}>
             <Button
               onClick={() => {
                 setSelectedHouse(row);
@@ -214,160 +170,135 @@ const MyHousesPage = () => {
               variant="secondary"
               size="sm"
             >
-              Edit
+              تعديل
             </Button>
-          </Col>
-          <Col>
             <Button
               variant="danger"
               onClick={() => handleDeleteHouse(row.id)}
               size="sm"
             >
-              Delete
+              إلغاء
             </Button>
           </Col>
         </Row>
-=======
-      cell: (row: HouseType) => (
-          <Row className="w-100">
-            <Col className={style.tableRow}>
-              <Button
-                  onClick={() => {
-                    setSelectedHouse(row);
-                    setModalShow(true);
-                  }}
-                  variant="secondary"
-                  size="sm"
-              >
-                تعديل
-              </Button>
-              <Button
-                  variant="danger"
-                  onClick={() => handleDeleteHouse(row.id)}
-                  size="sm"
-              >
-                إلغاء
-              </Button>
-            </Col>
-          </Row>
->>>>>>> origin/my-houses
       ),
     },
   ];
 
   return (
-      <div className="d-flex w-100 align-items-center flex-column p-4">
-        <h1 className="w-100 text-align-center my-4">منازلي</h1>
-        <Row className={style.customRow}>
-          <Col sm={8} xs={6} className={"p-0"}>
-            <Input
+    <div className="d-flex w-100 align-items-center flex-column p-4">
+      <h1 className="w-100 text-align-center my-4">منازلي</h1>
+      <Row className={style.customRow}>
+        <Col sm={8} xs={6} className={"p-0"}>
+          <Input
+            type="text"
+            placeholder="ابحث بالاسم"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-100 my-2"
+          />
+        </Col>
+        <Col sm={4} xs={6} className={style.customButton}>
+          <Button
+            onClick={() => {
+              setSelectedHouse(null);
+              setModalShow(true);
+            }}
+            variant="secondary"
+          >
+            أضف منزل
+          </Button>
+        </Col>
+      </Row>
+      <DataTable
+        className={style.houseTable}
+        columns={columns}
+        data={houses}
+        highlightOnHover
+        pointerOnHover
+        pagination
+        paginationPerPage={5}
+        paginationRowsPerPageOptions={[5, 10, 15, 20]}
+        noDataComponent="لم يتم العثور على أي منازل"
+        customStyles={tableStyle}
+      />
+      <Modal show={modalShow} onHide={() => setModalShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {selectedHouse ? "Edit House" : "Add House"}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleAddOrUpdateHouse}>
+            <Form.Group className="my-1" controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
                 type="text"
-                placeholder="ابحث بالاسم"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                className="w-100 my-2"
-            />
-          </Col>
-          <Col sm={4} xs={6} className={style.customButton}>
-            <Button
-                onClick={() => {
-                  setSelectedHouse(null);
-                  setModalShow(true);
-                }}
-                variant="secondary"
-            >
-              أضف منزل
+                defaultValue={selectedHouse?.name || ""}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="my-1" controlId="address">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={selectedHouse?.address || ""}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="my-1" controlId="phoneNumber">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={selectedHouse?.phoneNumber || ""}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="my-1" controlId="spaceForPeople">
+              <Form.Label>Space For People</Form.Label>
+              <Form.Control
+                type="number"
+                defaultValue={selectedHouse?.spaceForPeople || ""}
+                min="1"
+                required
+              />
+            </Form.Group>
+            <Form.Group className="my-1" controlId="additionalInformation">
+              <Form.Label>Additional Information</Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={selectedHouse?.additionnalInformation || ""}
+              />
+            </Form.Group>
+            <Form.Group className="my-1" controlId="taken">
+              <Form.Check
+                type="checkbox"
+                label="Taken"
+                defaultChecked={selectedHouse?.taken || false}
+              />
+            </Form.Group>
+            <Form.Group className="my-1" controlId="region">
+              <Form.Label>Select Region</Form.Label>
+              <Form.Control
+                as="select"
+                defaultValue={selectedHouse?.regionId || ""}
+                required
+              >
+                <option value="">Select a region</option>
+                {regions.map((region) => (
+                  <option key={region.id} value={region.id}>
+                    {region.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Button variant="primary" type="submit" className="my-2">
+              {selectedHouse ? "Save Changes" : "Add House"}
             </Button>
-          </Col>
-        </Row>
-        <DataTable className={style.houseTable}
-                   columns={columns}
-                   data={houses}
-                   highlightOnHover
-                   pointerOnHover
-                   pagination
-                   paginationPerPage={5}
-                   paginationRowsPerPageOptions={[5, 10, 15, 20]}
-                   noDataComponent="لم يتم العثور على أي منازل"
-                   customStyles={tableStyle}
-        />
-        <Modal show={modalShow} onHide={() => setModalShow(false)}>
-          <Modal.Header closeButton>
-            <Modal.Title>
-              {selectedHouse ? "Edit House" : "Add House"}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={handleAddOrUpdateHouse}>
-              <Form.Group className="my-1" controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                    type="text"
-                    defaultValue={selectedHouse?.name || ""}
-                    required
-                />
-              </Form.Group>
-              <Form.Group className="my-1" controlId="address">
-                <Form.Label>Address</Form.Label>
-                <Form.Control
-                    type="text"
-                    defaultValue={selectedHouse?.address || ""}
-                    required
-                />
-              </Form.Group>
-              <Form.Group className="my-1" controlId="phoneNumber">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control
-                    type="text"
-                    defaultValue={selectedHouse?.phoneNumber || ""}
-                    required
-                />
-              </Form.Group>
-              <Form.Group className="my-1" controlId="spaceForPeople">
-                <Form.Label>Space For People</Form.Label>
-                <Form.Control
-                    type="number"
-                    defaultValue={selectedHouse?.spaceForPeople || ""}
-                    min="1"
-                    required
-                />
-              </Form.Group>
-              <Form.Group className="my-1" controlId="additionalInformation">
-                <Form.Label>Additional Information</Form.Label>
-                <Form.Control
-                    type="text"
-                    defaultValue={selectedHouse?.additionnalInformation || ""}
-                />
-              </Form.Group>
-              <Form.Group className="my-1" controlId="taken">
-                <Form.Check
-                    type="checkbox"
-                    label="Taken"
-                    defaultChecked={selectedHouse?.taken || false}
-                />
-              </Form.Group>
-              <Form.Group className="my-1" controlId="region">
-                <Form.Label>Select Region</Form.Label>
-                <Form.Control
-                    as="select"
-                    defaultValue={selectedHouse?.regionId || ""}
-                    required
-                >
-                  <option value="">Select a region</option>
-                  {regions.map((region) => (
-                      <option key={region.id} value={region.id}>
-                        {region.name}
-                      </option>
-                  ))}
-                </Form.Control>
-              </Form.Group>
-              <Button variant="primary" type="submit" className="my-2">
-                {selectedHouse ? "Save Changes" : "Add House"}
-              </Button>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
   );
 };
 
