b254040f888975d952e72bc4299433b8067566f4
joumaaAli <ali.joumaa@net.usj.edu.lb>
2024-09-24 22:29:11 +0200

    route fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { createClient } from "@/utils/supabase/component";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Offcanvas from "react-bootstrap/Offcanvas";
 import styles from "./Navbar.module.scss";
 
 const NavBar = () => {
@@ -13,22 +13,22 @@ const NavBar = () => {
   const [user, setUser] = useState(null);
   const [role, setRole] = useState(null);
   const navLinks = [
-    {label: "الرئيسية", href: path.home},
-    {label: "اتصل بنا", href: path.contact},
-    {label: "المنازل", href: path.houses},
-    {label: "الملاجئ", href: path.myhouse},
+    { label: "الرئيسية", href: path.home },
+    { label: "اتصل بنا", href: path.contact },
+    { label: "المنازل", href: path.houses },
+    { label: "الملاجئ", href: path.shelters },
   ];
 
-  const adminLinks = [
-    {label: "لوحة التحكم", href: path.admin},
-  ];
+  const adminLinks = [{ label: "لوحة التحكم", href: path.admin }];
+
+  const authentiatedLinks = [{ label: "لوحة التحكم", href: path.myhouse }];
 
   useEffect(() => {
     const getSession = async () => {
-      const {data} = await supabase.auth.getSession();
+      const { data } = await supabase.auth.getSession();
       if (data?.session) {
         setUser(data.session.user);
-        if (data.session.user.app_metadata.role === 'super-admin') {
+        if (data.session.user.app_metadata.role === "super-admin") {
           setRole(data.session.user.app_metadata.role);
         }
       }
@@ -37,14 +37,14 @@ const NavBar = () => {
     // Initial session fetch
     getSession();
 
-    const {data: authListener} = supabase.auth.onAuthStateChange(
-        (event, session) => {
-          if (session) {
-            setUser(session.user);
-          } else {
-            setUser(null);
-          }
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (session) {
+          setUser(session.user);
+        } else {
+          setUser(null);
         }
+      }
     );
 
     // Cleanup subscription on component unmount
@@ -56,78 +56,99 @@ const NavBar = () => {
   const isAdmin = role === "super-admin";
 
   return (
-      <Navbar bg="light" expand="lg" className={styles.navbar} dir="rtl">
-        <Container className={styles.navbarContainer}>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Navbar.Offcanvas
-                id="offcanvasNavbar-expand-lg"
-                aria-labelledby="offcanvasNavbarLabel-expand-lg"
-                placement="end"
-                className={styles.offcanvas}
-            >
-              <Offcanvas.Header closeButton className={styles.offcanvasHeader}>
-                <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg" className="ms-auto">
-                  المحتوى
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className=" d-flex align-items-start">
-                  {navLinks.map((link, index) => (
-                      <Nav.Link
-                          key={index}
-                          className={`${styles.navLink} ${
-                              router.pathname === link.href ? styles.active : ""
-                          }`}
-                          href={link.href}
-                      >
-                        {link.label}
-                      </Nav.Link>
+    <Navbar bg="light" expand="lg" className={styles.navbar} dir="rtl">
+      <Container className={styles.navbarContainer}>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-lg"
+            aria-labelledby="offcanvasNavbarLabel-expand-lg"
+            placement="end"
+            className={styles.offcanvas}
+          >
+            <Offcanvas.Header closeButton className={styles.offcanvasHeader}>
+              <Offcanvas.Title
+                id="offcanvasNavbarLabel-expand-lg"
+                className="ms-auto"
+              >
+                المحتوى
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className=" d-flex align-items-start">
+                {navLinks.map((link, index) => (
+                  <Nav.Link
+                    key={index}
+                    className={`${styles.navLink} ${
+                      router.pathname === link.href ? styles.active : ""
+                    }`}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Nav.Link>
+                ))}
+
+                {user &&
+                  !isAdmin &&
+                  authentiatedLinks.map((link, index) => (
+                    <Nav.Link
+                      key={index}
+                      className={`${styles.navLink} ${
+                        router.pathname === link.href ? styles.active : ""
+                      }`}
+                      href={link.href}
+                    >
+                      {link.label}
+                    </Nav.Link>
                   ))}
 
-                  {user &&
-                      isAdmin &&
-                      adminLinks.map((link, index) => (
-                          <Nav.Link
-                              key={index}
-                              className={`${styles.navLink} ${
-                                  router.pathname === link.href ? styles.active : ""
-                              }`}
-                              href={link.href}
-                          >
-                            {link.label}
-                          </Nav.Link>
-                      ))}
+                {user &&
+                  isAdmin &&
+                  adminLinks.map((link, index) => (
+                    <Nav.Link
+                      key={index}
+                      className={`${styles.navLink} ${
+                        router.pathname === link.href ? styles.active : ""
+                      }`}
+                      href={link.href}
+                    >
+                      {link.label}
+                    </Nav.Link>
+                  ))}
 
-                  {user && (
-                      <>
-                      <Nav.Link className={styles.navLink}
-                          onClick={() => {
-                            HandleLogout();
-                            router.push(path.home);
-                          }}
-                      >
-                        تسجيل الخروج
-                      </Nav.Link>
-                      </>
-                  )}
+                {user && (
+                  <>
+                    <Nav.Link
+                      className={styles.navLink}
+                      onClick={() => {
+                        HandleLogout();
+                        router.push(path.home);
+                      }}
+                    >
+                      تسجيل الخروج
+                    </Nav.Link>
+                  </>
+                )}
 
-                  {!user && (
-                      <>
-                        <Nav.Link className={styles.navLink} href={path.auth.login}>
-                          تسجيل الدخول
-                        </Nav.Link>
-                        <Nav.Link className={styles.navLink} href={path.auth.register}>
-                          إنشاء حساب
-                        </Nav.Link>
-                      </>
-                  )}
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+                {!user && (
+                  <>
+                    <Nav.Link className={styles.navLink} href={path.auth.login}>
+                      تسجيل الدخول
+                    </Nav.Link>
+                    <Nav.Link
+                      className={styles.navLink}
+                      href={path.auth.register}
+                    >
+                      إنشاء حساب
+                    </Nav.Link>
+                  </>
+                )}
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -14,4 +14,5 @@ export const path = {
   records: "/records",
   myhouse: "/my-house",
   houses: "/houses",
+  shelters: "/shelters",
 };
